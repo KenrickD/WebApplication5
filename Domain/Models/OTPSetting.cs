533dbf73d5e0b731cd58c45fff531a9214382ca6
KenDnG <daniswara.kenrick@gmail.com>
2025-08-12 13:00:22 +0700

    Add project files.

---
+++ Domain/Models/OTPSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    [Table("TB_OTPSetting")]
+    public class OTPSetting
+    {
+        public Guid OTPId { get; set; }
+        public string? OTPAction { get; set; }
+        public bool Email { get; set; }
+        public bool Whatsapp { get; set; }
+
+    }
+}
